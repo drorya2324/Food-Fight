df78ee193debfdfa7391e624d59c20738a31a028
drorya2324 <drorminush@gmail.com>
2020-05-18 15:09:49 +0300

    improved movement, throwing food, Level1 scene

---
+++ Scripts/character.gd
@@ -0,0 +1,21 @@
+extends KinematicBody
+
+const PROJECTILE_SPEED = 50
+
+var ammo_types = {}
+
+func _enter_tree():
+	ammo_types = FileGrabber.get_files("res://Scenes/Ammo/Ammo_Models/")
+	randomize()
+
+
+func fire():
+	var random_bullet = ammo_types[randi() % ammo_types.size()]
+	var bullet = load(random_bullet).instance()
+	add_child(bullet)
+	bullet.set_as_toplevel( true)
+	bullet.global_transform = $Forward.global_transform
+	var character_forward = get_global_transform().basis[2].normalized()
+	bullet.set_linear_velocity(character_forward * PROJECTILE_SPEED)
+	bullet.add_collision_exception_with(self)
+
